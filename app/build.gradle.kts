5c4692f99872cdabd0ef3b2afc38718d0e58012b
Jaideep <jaideepindia99@gmail.com>
2025-05-25 22:09:32 +0530

    Update:gg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android) // Required for kotlinOptions
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android") // <-- Classic plugin for Kotlin Android
 }
 
 android {
